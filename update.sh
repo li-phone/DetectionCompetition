e36ba6724d1029bc883c4078aa7821a3304100e5
liphone <974122407@qq.com>
2020-01-31 16:28:47 +0800

    commit in 2020-01-31 16:28:47 by liphone

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 pip freeze > requirements.txt
 git add .
+git add *
 time_str=$(date "+%Y-%m-%d %H:%M:%S")
 git commit -m "commit in ${time_str} by `whoami`"
 git push origin master
