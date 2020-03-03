41336c09897a4ae90d3580d0253811872ca505f5
liphone <974122407@qq.com>
2020-03-03 22:27:36 +0800

    commit in 2020-03-03 22:27:36 by liphone

--- DefectNet/utilities/batch_train_for_fabric.py
+++ DefectNet/utilities/batch_train_for_fabric.py
@@ -3,11 +3,11 @@ from train_with_tricks import BatchTrain
 
 def main():
     batrian = BatchTrain(cfg_path='../config_alcohol/cascade_rcnn_r50_fpn_1x/fabric.py', data_mode='test')
-    batrian.multi_scale_train(img_scale=[(2446, 1000)])
     batrian.multi_scale_train(img_scale=[(2446 / 2, 1000 / 2)], ratio_range=[0.5, 1.5])
     batrian.multi_scale_train(img_scale=[(2446 / 2, 1000 / 2), (1333, 800)], multiscale_mode='value')
     batrian.multi_scale_train(img_scale=[(2446 / 2, 1000 / 2), (1333, 800)])
     batrian.multi_scale_train(img_scale=[(2446 / 2, 1000 / 2)])
+    batrian.multi_scale_train(img_scale=[(2446, 1000)])
 
 
 if __name__ == '__main__':
