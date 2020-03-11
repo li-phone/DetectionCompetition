a280cc9ac4d637c21683599a5830a5f220b71f48
liphone <974122407@qq.com>
2020-03-11 16:37:42 +0800

    commit in 2020-03-11 16:37:42 by liphone

--- DefectNet/utilities/batch_train_for_fabric.py
+++ DefectNet/utilities/batch_train_for_fabric.py
@@ -10,7 +10,7 @@ def main():
     # fabric_train.anchor_cluster_train(anchor_ratios=[0.12, 1.0, 4.43])
     # fabric_train.anchor_cluster_train(anchor_ratios=[0.04, 0.28, 1.0, 4.43, 8.77])
     # fabric_train.anchor_cluster_train(anchor_ratios=[0.04, 0.14, 0.32, 0.69, 1.0, 4.77, 8.84])
-    fabric_train.anchor_cluster_train(anchor_ratios=[0.04, 0.14, 0.27, 0.32, 0.69, 1.0, 3.0, 5.71, 10.22])
+    # fabric_train.anchor_cluster_train(anchor_ratios=[0.04, 0.14, 0.27, 0.32, 0.69, 1.0, 3.0, 5.71, 10.22])
     # batrian.baseline_train()
     # batrian.multi_scale_train(img_scale=[(2446 / 2, 1000 / 2)], ratio_range=[0.5, 1.5])
     # batrian.multi_scale_train(img_scale=[(2446 / 2, 1000 / 2), (1333, 800)], multiscale_mode='value')
