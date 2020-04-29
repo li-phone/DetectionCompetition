8561b89581d4b3de64ea4272bfc25465f4a26379
liphone <974122407@qq.com>
2020-04-29 23:57:38 +0800

    update download.sh commit in 2020-04-29 23:57:38 by LiFeng

--- mmdetection/tools/utils/download_dataset.sh
+++ mmdetection/tools/utils/download_dataset.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 filename="download_list.txt"
-save_dir="./"
-if ${1}!="":
-  save_dir=${1}
-echo "save_dir:${save_dir}"
+save_dir=$1
+
 while read line
 do
     array=(${line//,/ })
