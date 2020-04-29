d373bc534bab1e67de139728ede82d738628ca77
liphone <974122407@qq.com>
2020-04-29 23:32:29 +0800

    add cbnet commit in 2020-04-29 23:32:29 by LiFeng

--- mmdetection/tools/demo_train.py
+++ mmdetection/tools/demo_train.py
@@ -4,7 +4,7 @@ from batch_train import BatchTrain
 def train_models(test_status=10):
     BatchTrain(cfg_path='../configs/cartoon_face/retinanet_r50_fpn_1x.py',
                data_mode='val', train_sleep_time=0, test_sleep_time=test_status).common_train()
-
+import torchvision.models.resnet
 
 def main():
     train_models()
