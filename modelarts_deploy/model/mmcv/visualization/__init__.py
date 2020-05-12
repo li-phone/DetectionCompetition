0693018f3490337ec0a313d09f32888713796e2a
liphone <974122407@qq.com>
2020-05-12 21:35:09 +0800

    add mmdet without mmcv commit in 2020-05-12 21:35:09 by liphone

---
+++ modelarts_deploy/model/mmcv/visualization/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) Open-MMLab. All rights reserved.
+from .color import Color, color_val
+from .image import imshow, imshow_bboxes, imshow_det_bboxes
+
+__all__ = [
+    'Color', 'color_val', 'imshow', 'imshow_bboxes', 'imshow_det_bboxes'
+]
