2e22efda923d12efa3b6fb798d8f7bef387c0a66
liphone <974122407@qq.com>
2020-05-14 18:23:46 +0800

    add do_submit.sh commit in 2020-05-14 18:23:46 by liphone

--- mmdetection/tools/do_submit.sh
+++ mmdetection/tools/do_submit.sh
@@ -1,5 +1,6 @@
 DIR="../work_dirs/breast/cascade_rcnn_x101_64x4d_fpn_1x_groie+multiscale+softnms+nobg"
 TEST_DIR="$HOME/undone-work/data/detection/breast/annotations/"
 python do_submit.py ${TEST_DIR}/test_data_A.json "${DIR}/data_mode=test+.bbox.json" ${DIR}/submit_nobg_groie --convert filter_id=0 cvt_img_id=json cvt_box=xywh2xyxy cvt_score=append cvt_cat_id=True
-zip -q -r submit_nobg_groie.zip ${DIR}/submit_nobg_groie
-mv submit_nobg_groie.zip ${DIR}
+cd $DIR
+zip -q -r submit_nobg_groie.zip submit_nobg_groie
+echo "OK!"
