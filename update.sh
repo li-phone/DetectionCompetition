1d92d68f4eb27a1ada550f56bd4c292c90e4159c
liphone <974122$07@qq.com>
2021-03-03 01:10:25 +0800

    add git branch commit in 2021-03-03 01:10:25 by 97412

--- update.sh
+++ update.sh
@@ -5,5 +5,6 @@ time_str=$(date "+%Y-%m-%d %H:%M:%S")
 git commit -m "${1} commit in ${time_str} by `whoami`"
 git push origin master
 git checkout li-phone
+git add .
 git push github li-phone
 exit
