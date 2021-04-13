da9e79b1d62822939c067c986b0dbea03a51dd10
li-phone <974122407@qq.com>
2021-04-13 19:35:18 +0800

    sync to li-phone branch and delete master branch commit in 2021-04-13 19:35:18 by 97412

--- update.sh
+++ update.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
-#pip freeze > requirements.txt
+# pip freeze > requirements.txt
+
+# checkout to gitee li-phone branch
+git checkout li-phone
 git add .
 time_str=$(date "+%Y-%m-%d %H:%M:%S")
 git commit -m "${1} commit in ${time_str} by `whoami`"
 git push origin master
+
+# checkout to github li-phone branch
 git checkout li-phone
-git add *
+git add .
 git commit -m "${1} commit in ${time_str} by `whoami`"
 git push github li-phone
 exit
