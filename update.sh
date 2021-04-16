9dc6ec24d155f87d14d4d0efcf5719cce59bbf75
li-phone <974122407@qq.com>
2021-04-16 21:12:40 +0800

    add li-phone branch commit in 2021-04-16 21:12:40 by 97412

--- update.sh
+++ update.sh
@@ -3,6 +3,8 @@
 git add .
 time_str=$(date "+%Y-%m-%d %H:%M:%S")
 git commit -m "${1} commit in ${time_str} by `whoami`"
-git push -f gitee master
-git push -f github master
+# git push -f gitee master
+# git push -f github master
+git push --set-upstream github li-phone -f
+git push --set-upstream gitee li-phone -f
 exit
